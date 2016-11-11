b9b433feb74a07e7e551e4ebd4ec6596bc790be3
Eric Fredine <eric.fredine@gmail.com>
2016-11-11 19:20:58 +0000

    Create a function for setting a user as logged in which is called on log in and registration.  Added some comments to auth_routes.

--- auth_routes.js
+++ auth_routes.js
@@ -21,6 +21,22 @@ module.exports = function(app) {
     next();
   });
 
+  /**
+   * Mark a user as logged in by adding a user record into the session.  Only email and id are stored in the session.
+   * @param {Object} req the http request object
+   * @param {Object} userRecord object
+   */
+  function setLoggedIn(req, userRecord) {
+    req.session.userRecord = {email: userRecord.email, id: userRecord.id};
+  }
+
+  /**
+   * Authenticates if email is found and bycrypt'ed password compares.
+   * @param  {Object} req object
+   * @param  {Object} res object
+   * @param  {Function} onResult called once authentication is completed
+   * @return {undefined}
+   */
   function authenticate(req, res, onResult) {
     let email = req.body.email;
     let userId = models.findUserId("email", email);
@@ -31,11 +47,12 @@ module.exports = function(app) {
     let userRecord = models.getUserForId(userId);
     bcrypt.compare(req.body.password, userRecord.password, (err, result) =>{
       if(result) {
-        req.session.userRecord = {email: userRecord.email, id: userRecord.id};
+        setLoggedIn(req, userRecord);
       }
       onResult(err, result);
     });
   }
+
   /**
    * Email and password can't be blank and the email must not already be registered.
    * @param  {String} email
@@ -55,6 +72,9 @@ module.exports = function(app) {
     }
   }
 
+  // ROUTES -----------------------------------------------------------------
+
+  // Return the registration form or  redirect to home if the user is already logged in.
   app.get("/register", (req, res) =>{
     if(loggedInUser(req, res)){
       res.redirect("/");
@@ -63,23 +83,29 @@ module.exports = function(app) {
     }
   });
 
+  // Process a registration request.
   app.post("/register", (req, res) =>{
     let errorMessage = checkIfValid(req.body.email, req.body.password);
+
+    // If invalid, render an error message.
     if(errorMessage) {
       res.render('register', {errorMessage: errorMessage});
       return;
     }
+
+    // If valid, hash the password, store it, mark the user logged in and redirect to home.
     bcrypt.hash(req.body.password, saltRounds, (err, hashedPassword) => {
       let userId = models.insertUser({
         email: req.body.email,
         password: hashedPassword
       });
       let userRecord = models.getUserForId(userId);
-      req.session.userRecord = {email: userRecord.email, id: userRecord.id};
+      setLoggedIn(req, userRecord);
       res.redirect("/");
     });
   });
 
+  // Return the login form or redirect to home if the user is already logged in.
   app.get("/login", (req, res) =>{
     if(loggedInUser(req, res)) {
       res.redirect("/");
@@ -88,20 +114,27 @@ module.exports = function(app) {
     }
   });
 
+  // Process a log in request by authenticating.
   app.post("/login", (req, res) => {
     authenticate(req, res, (err, result) => {
+
+      // If successfully authenticated, redirect to home.
       if(result) {
         res.redirect("/");
+
+      // If uathentication fails, render an error (that's what the spec says!).
       } else {
         renderForbidden(req, res);
       }
     });
   });
 
+  // If a logged in user tries to get the logout page they are just redirected to home.  Unauthorized users get an error.
   app.get("/logout", blockUnauthorized, (req, res) => {
     res.redirect("/");
   });
 
+  // Process a logout from a logged in user by setting the session to null.  Unauthorized users get an error.
   app.post("/logout", blockUnauthorized, (req, res) => {
     req.session = null;
     res.redirect("/");
